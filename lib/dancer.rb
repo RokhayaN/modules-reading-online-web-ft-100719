5d30356c75632500759840eee4c678d578a8dcec
Rokhaya  Ndiaye <rokhayamuslima1@gmail.com>
2019-10-28 05:44:05 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,3 +1,11 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 class Dancer < Dance
+  include Dance
+ 
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+  end
+end
